48823f37a407b87d066ffbbee191968f161ab24f
DainelCornejoStona <danicornejof@gmail.com>
2022-10-24 10:10:48 +0200

    commit

--- js/ejercicio01.js
+++ js/ejercicio01.js
@@ -1,6 +1,21 @@
+document.getElementById("numero").addEventListener("dblclick",triple);
+document.getElementById("numero").addEventListener("mouseover",ratondentro);
+document.getElementById("numero").addEventListener("mouseout",ratonfuera);
+/*
 document.getElementById("numero").ondblclick=triple;
+document.getElementById("numero").onmouseover=ratondentro;
+document.getElementById("numero").onmouseout=ratonfuera;
+*/
 function triple(){
     var numero = document.getElementById("numero").textContent;
     var num= new Number(numero);
     alert(num*3);   
+}
+function ratondentro(){
+    document.getElementById("numero").style.color = "orange";
+    document.getElementById("numero").style.font= "30px sans-serif";
+}
+function ratonfuera(){
+    document.getElementById("numero").style.color = "black";
+    document.getElementById("numero").style.font= "18px liberation-serif";
 }
--- js/ejercicio02.js
+++ js/ejercicio02.js
@@ -1,2 +1,42 @@
-function muestraOculta() {
+document.getElementById("enlace_1").addEventListener("click",muestraOculta);
+document.getElementById("enlace_2").addEventListener("click",muestraOculta);
+document.getElementById("enlace_3").addEventListener("click",muestraOculta);
+
+function muestraOculta(e) {
+    switch (e.target.id) {
+        case "enlace_1":
+            var x = document.getElementById("contenidos_1");
+        if(x.style.display === "none"){
+            x.style.display = "block";
+            document.getElementById("enlace_1").innerHTML="Ocultar contenidos";
+        }
+        else{
+            x.style.display = "none";
+            document.getElementById("enlace_1").innerHTML="Abrir contenidos";
+        }
+        break;
+        case "enlace_2":
+            var x = document.getElementById("contenidos_2");
+        if(x.style.display === "none"){
+            x.style.display = "block";
+            document.getElementById("enlace_2").innerHTML="Ocultar contenidos";
+        }
+        else{
+            x.style.display = "none";
+            document.getElementById("enlace_2").innerHTML="Abrir contenidos";
+        }
+        break;
+        case "enlace_3":
+            var x = document.getElementById("contenidos_3");
+        if(x.style.display === "none"){
+            x.style.display = "block";
+            document.getElementById("enlace_3").innerHTML="Ocultar contenidos";
+        }
+        else{
+            x.style.display = "none";
+            document.getElementById("enlace_3").innerHTML="Abrir contenidos";
+        }
+        break;
+
+    }
 }
