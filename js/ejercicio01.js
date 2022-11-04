b52abf5927e616d64256841391cae92962f3a2ee
DainelCornejoStona <danicornejof@gmail.com>
2022-11-04 08:39:26 +0100

    commit

--- js/ejercicio01.js
+++ js/ejercicio01.js
@@ -9,13 +9,16 @@ document.getElementById("numero").onmouseout=ratonfuera;
 function triple(){
     var numero = document.getElementById("numero").textContent;
     var num= new Number(numero);
-    alert(num*3);   
+    document.getElementById("numero").textContent= num*3;
+       
 }
 function ratondentro(){
     document.getElementById("numero").style.color = "orange";
+    document.getElementById("numero").style.backgroundColor = "blue";
     document.getElementById("numero").style.font= "30px sans-serif";
 }
 function ratonfuera(){
     document.getElementById("numero").style.color = "black";
+    document.getElementById("numero").style.backgroundColor = "white";
     document.getElementById("numero").style.font= "18px liberation-serif";
 }
